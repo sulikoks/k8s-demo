136b77d2b2fdd885a1f18d2ad4405830b1d25b8a
suliko <79033456672sd@gmail.com>
2022-08-26 19:04:55 +0400

    v1.0.22

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   <div>HOSTNAME: ${HOSTNAME}</div>
   <div>Version ${packageJson.version}</div>
   <div>ENVTEST ${process.env.DEMO_GREETING}</div>
-  <div>ENV ${process.env}</div>
+  <div>ENV ${JSON.stringify(process.env)}</div>
 </div>
   `);
 });
